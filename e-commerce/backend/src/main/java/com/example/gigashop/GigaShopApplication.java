555590ec848ffa9c150072e9d0cf7ec81896c061
ShubhamNagwanshi <142484423+ShubhamNagwanshi@users.noreply.github.com>
2025-03-31 00:53:32 +0530

    Add e-commerce/backend/src/main/java/com/example/gigashop/GigaShopApplication.java 1743362609995

---
+++ e-commerce/backend/src/main/java/com/example/gigashop/GigaShopApplication.java
@@ -0,0 +1,11 @@
+package com.example.gigashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigaShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GigaShopApplication.class, args);
+    }
+}
